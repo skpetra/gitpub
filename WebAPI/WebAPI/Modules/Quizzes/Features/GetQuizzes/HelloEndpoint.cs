c03069caf01d6fb485410d3d2c0c6e962c4643d0
Petra Škrabo <pskrabo@span.eu>
2025-07-18 18:10:03 +0200

    Initial project setup

---
+++ WebAPI/WebAPI/Modules/Quizzes/Features/GetQuizzes/HelloEndpoint.cs
@@ -0,0 +1,22 @@
+﻿namespace WebAPI.Modules.Quizzes.Features.GetQuizzes;
+
+using FastEndpoints;
+
+public class HelloEndpoint : EndpointWithoutRequest<string>
+{
+    public override void Configure()
+    {
+        Get("/hello");
+        AllowAnonymous();
+        Description(b => b
+            .WithSummary("Returns a hello message")
+            .WithDescription("This endpoint returns a simple hello message for testing purposes.")
+        );
+    }
+
+    public override Task HandleAsync(CancellationToken ct)
+    {
+        Response = "Hello, FastEndpoints!";
+        return Task.CompletedTask;
+    }
+}
