4812abb5efd51d8c9307fca063c52a7d88971ba7
Petra Škrabo <petraskrabo@hotmail.com>
2025-07-23 15:40:03 +0200

    Validation rules (#6)

--- WebAPI/WebAPI.BL/Services/Interfaces/IQuestionService.cs
+++ WebAPI/WebAPI.BL/Services/Interfaces/IQuestionService.cs
@@ -6,4 +6,6 @@ public interface IQuestionService
 {
     Task<bool> DoAllQuestionsExistAsync(int[] questionIds, CancellationToken ct = default);
     Task<IEnumerable<Question>> GetQuestionsByIdsAsync(int[] questionIds, CancellationToken ct = default);
+    Task<bool> DoesQuestionExistByTextAsync(string text, CancellationToken ct = default);
+    Task<bool> DoesQuestionExistByIdAsync(int id, CancellationToken ct = default);
 }
---
+++ WebAPI/WebAPI/Core/Validation/BaseQuestionValidator.cs
@@ -0,0 +1,28 @@
+﻿using FastEndpoints;
+using FluentValidation;
+using WebAPI.BL.Models.Questions;
+using WebAPI.BL.Services.Interfaces;
+using WebAPI.Constants;
+
+namespace WebAPI.Core.Validation;
+
+public class BaseQuestionValidator : Validator<QuestionAddDto>
+{
+    public BaseQuestionValidator()
+    {
+        RuleFor(q => q.Text)
+            .NotEmpty()
+            .MustAsync(QuestionDoesNotExist)
+                .WithMessage(ErrorConstants.Question.QuestionAlreadyExists);
+
+        RuleFor(q => q.Answer)
+            .NotEmpty();
+    }
+
+    private async Task<bool> QuestionDoesNotExist(string text, CancellationToken ct)
+    {
+        var questionService = Resolve<IQuestionService>();
+
+        return !await questionService.DoesQuestionExistByTextAsync(text, ct);
+    }
+}
